fc85558ef28ba643f1e8ec3477dcc16195b07964
Jeel Bhatti <jeelbhatti9@gmail.com>
2024-02-16 13:08:47 +0530

    issue fix

--- android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
+++ android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
@@ -13,7 +13,9 @@ import com.otaliastudios.transcoder.strategy.RemoveTrackStrategy
 import com.otaliastudios.transcoder.strategy.TrackStrategy
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
-import com.otaliastudios.transcoder.internal.Logger
+import com.otaliastudios.transcoder.internal.utils.Logger
+import com.otaliastudios.transcoder.resize.AtMostResizer
+import com.otaliastudios.transcoder.resize.ExactResizer
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -33,7 +35,7 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
     private var _channel: MethodChannel? = null
     private val TAG = "VideoCompressPlugin"
     private val LOG = Logger(TAG)
-    private var transcodeFuture:Future<Void>? = null
+    private var transcodeFuture: Future<Void>? = null
     var channelName = "video_compress"
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
@@ -50,31 +52,44 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
                 val path = call.argument<String>("path")
                 val quality = call.argument<Int>("quality")!!
                 val position = call.argument<Int>("position")!! // to long
-                ThumbnailUtility(channelName).getByteThumbnail(path!!, quality, position.toLong(), result)
+                ThumbnailUtility(channelName).getByteThumbnail(
+                    path!!,
+                    quality,
+                    position.toLong(),
+                    result
+                )
             }
+
             "getFileThumbnail" -> {
                 val path = call.argument<String>("path")
                 val quality = call.argument<Int>("quality")!!
                 val position = call.argument<Int>("position")!! // to long
-                ThumbnailUtility("video_compress").getFileThumbnail(context, path!!, quality,
-                        position.toLong(), result)
+                ThumbnailUtility("video_compress").getFileThumbnail(
+                    context, path!!, quality,
+                    position.toLong(), result
+                )
             }
+
             "getMediaInfo" -> {
                 val path = call.argument<String>("path")
                 result.success(Utility(channelName).getMediaInfoJson(context, path!!).toString())
             }
+
             "deleteAllCache" -> {
                 result.success(Utility(channelName).deleteAllCache(context, result));
             }
+
             "setLogLevel" -> {
                 val logLevel = call.argument<Int>("logLevel")!!
                 Logger.setLogLevel(logLevel)
                 result.success(true);
             }
+
             "cancelCompression" -> {
                 transcodeFuture?.cancel(true)
                 result.success(false);
             }
+
             "compressVideo" -> {
                 val path = call.argument<String>("path")!!
                 val quality = call.argument<Int>("quality")!!
@@ -82,11 +97,16 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
                 val startTime = call.argument<Int>("startTime")
                 val duration = call.argument<Int>("duration")
                 val includeAudio = call.argument<Boolean>("includeAudio") ?: true
-                val frameRate = if (call.argument<Int>("frameRate")==null) 30 else call.argument<Int>("frameRate")
+                val frameRate =
+                    if (call.argument<Int>("frameRate") == null) 30 else call.argument<Int>("frameRate")
+                val bitRate = call.argument<Int>("bitRate")
+                val outputWidth = call.argument<Int>("outputWidth")
+                val outputHeight = call.argument<Int>("outputHeight")
 
                 val tempDir: String = context.getExternalFilesDir("video_compress")!!.absolutePath
                 val out = SimpleDateFormat("yyyy-MM-dd hh-mm-ss").format(Date())
-                val destPath: String = tempDir + File.separator + "VID_" + out + path.hashCode() + ".mp4"
+                val destPath: String =
+                    tempDir + File.separator + "VID_" + out + path.hashCode() + ".mp4"
 
                 var videoTrackStrategy: TrackStrategy = DefaultVideoStrategy.atMost(340).build();
                 val audioTrackStrategy: TrackStrategy
@@ -94,36 +114,177 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
                 when (quality) {
 
                     0 -> {
-                      videoTrackStrategy = DefaultVideoStrategy.atMost(720).build()
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(720))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(720))
+                                .build()
+                        }
                     }
 
                     1 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(360).build()
+
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(360))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(360))
+                                .build()
+                        }
                     }
+
                     2 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(640).build()
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(640))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(640))
+                                .build()
+                        }
                     }
+
                     3 -> {
 
                         assert(value = frameRate != null)
                         videoTrackStrategy = DefaultVideoStrategy.Builder()
-                                .keyFrameInterval(3f)
-                                .bitRate(1280 * 720 * 4.toLong())
-                                .frameRate(frameRate!!) // will be capped to the input frameRate
-                                .build()
+                            .keyFrameInterval(3f)
+                            .bitRate(1280 * 720 * 4.toLong())
+                            .frameRate(frameRate!!) // will be capped to the input frameRate
+                            .build()
                     }
+
                     4 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(480, 640).build()
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(480, 640))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(480, 640))
+                                .build()
+                        }
                     }
+
                     5 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(540, 960).build()
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(540, 960))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(540, 960))
+                                .build()
+                        }
                     }
+
                     6 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(720, 1280).build()
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(720, 1280))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(720, 1280))
+                                .build()
+                        }
                     }
+
                     7 -> {
-                        videoTrackStrategy = DefaultVideoStrategy.atMost(1080, 1920).build()
-                    }                    
+                        assert(value = frameRate != null)
+                        videoTrackStrategy = if (bitRate == null) {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(1080, 1920))
+                                .build()
+                        } else {
+                            DefaultVideoStrategy.Builder()
+                                .keyFrameInterval(3f)
+                                .bitRate(bitRate.toLong())
+                                .frameRate(frameRate!!) // will be capped to the input frameRate
+                                .addResizer(AtMostResizer(1080, 1920))
+                                .build()
+                        }
+                    }
+
+                    8 -> {
+
+                        assert(value = frameRate != null)
+                        if (outputWidth != null && outputHeight != null) {
+                            videoTrackStrategy = if (bitRate == null) {
+                                DefaultVideoStrategy.Builder()
+                                    .keyFrameInterval(3f)
+                                    .frameRate(frameRate!!) // will be capped to the input frameRate
+                                    .addResizer(ExactResizer(outputWidth, outputHeight))
+                                    .build()
+                            } else {
+                                DefaultVideoStrategy.Builder()
+                                    .keyFrameInterval(3f)
+                                    .bitRate(bitRate.toLong())
+                                    .frameRate(frameRate!!) // will be capped to the input frameRate
+                                    .addResizer(ExactResizer(outputWidth, outputHeight))
+                                    .build()
+                            }
+                        } else {
+                            videoTrackStrategy = if (bitRate == null) {
+                                DefaultVideoStrategy.Builder()
+                                    .keyFrameInterval(3f)
+                                    .frameRate(frameRate!!) // will be capped to the input frameRate
+                                    .addResizer(AtMostResizer(720, 1280))
+                                    .build()
+                            } else {
+                                DefaultVideoStrategy.Builder()
+                                    .keyFrameInterval(3f)
+                                    .bitRate(bitRate.toLong())
+                                    .frameRate(frameRate!!) // will be capped to the input frameRate
+                                    .addResizer(AtMostResizer(720, 1280))
+                                    .build()
+                            }
+                        }
+                    }
                 }
 
                 audioTrackStrategy = if (includeAudio) {
@@ -138,41 +299,47 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
                     RemoveTrackStrategy()
                 }
 
-                val dataSource = if (startTime != null || duration != null){
+                val dataSource = if (startTime != null || duration != null) {
                     val source = UriDataSource(context, Uri.parse(path))
-                    TrimDataSource(source, (1000 * 1000 * (startTime ?: 0)).toLong(), (1000 * 1000 * (duration ?: 0)).toLong())
-                }else{
+                    TrimDataSource(
+                        source,
+                        (1000 * 1000 * (startTime ?: 0)).toLong(),
+                        (1000 * 1000 * (duration ?: 0)).toLong()
+                    )
+                } else {
                     UriDataSource(context, Uri.parse(path))
                 }
 
 
                 transcodeFuture = Transcoder.into(destPath!!)
-                        .addDataSource(dataSource)
-                        .setAudioTrackStrategy(audioTrackStrategy)
-                        .setVideoTrackStrategy(videoTrackStrategy)
-                        .setListener(object : TranscoderListener {
-                            override fun onTranscodeProgress(progress: Double) {
-                                channel.invokeMethod("updateProgress", progress * 100.00)
-                            }
-                            override fun onTranscodeCompleted(successCode: Int) {
-                                channel.invokeMethod("updateProgress", 100.00)
-                                val json = Utility(channelName).getMediaInfoJson(context, destPath)
-                                json.put("isCancel", false)
-                                result.success(json.toString())
-                                if (deleteOrigin) {
-                                    File(path).delete()
-                                }
-                            }
+                    .addDataSource(dataSource)
+                    .setAudioTrackStrategy(audioTrackStrategy)
+                    .setVideoTrackStrategy(videoTrackStrategy)
+                    .setListener(object : TranscoderListener {
+                        override fun onTranscodeProgress(progress: Double) {
+                            channel.invokeMethod("updateProgress", progress * 100.00)
+                        }
 
-                            override fun onTranscodeCanceled() {
-                                result.success(null)
+                        override fun onTranscodeCompleted(successCode: Int) {
+                            channel.invokeMethod("updateProgress", 100.00)
+                            val json = Utility(channelName).getMediaInfoJson(context, destPath)
+                            json.put("isCancel", false)
+                            result.success(json.toString())
+                            if (deleteOrigin) {
+                                File(path).delete()
                             }
+                        }
 
-                            override fun onTranscodeFailed(exception: Throwable) {
-                                result.success(null)
-                            }
-                        }).transcode()
+                        override fun onTranscodeCanceled() {
+                            result.success(null)
+                        }
+
+                        override fun onTranscodeFailed(exception: Throwable) {
+                            result.success(null)
+                        }
+                    }).transcode()
             }
+
             else -> {
                 result.notImplemented()
             }
